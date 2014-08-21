91c97a65562fc12d8035b866d8db7a0c20bad4e8
Zoltan Olah <zol@percolatestudio.com>
2014-08-21 15:23:25 -0700

    Adds s3 deployment via grunt

--- gruntfile.js
+++ gruntfile.js
@@ -19,11 +19,40 @@ module.exports = function(grunt) {
         files: ['css/**/*.less', 'css/*.less', 'style.less'],
         tasks: ['less']
       }
+    },
+    aws: grunt.file.readJSON(process.env.HOME + '/secret-config/percolate-s3-deploy.json'),
+    s3: {
+      options: {
+        key: '<%= aws.key %>',
+        secret: '<%= aws.secret %>',
+        bucket: 'meetsous.com',
+        region: 'us-west-1',
+        access: 'public-read',
+        headers: {
+          // Two Year cache policy (1000 * 60 * 60 * 24 * 730)
+          "Cache-Control": "max-age=630720000, public",
+          "Expires": new Date(Date.now() + 63072000000).toUTCString()
+        },
+        // maxOperations: 20,
+        verify: true
+      },
+      deploy: {
+        sync: [
+          { src: 'fonts/*', dest: 'fonts' },
+          { src: 'img/*', dest: 'img' },
+          { src: 'js/*', dest: 'js' },
+          { src: '*.png', dest: '/' },
+          { src: 'index.html', dest: 'index.html' },
+          { src: 'robots.txt', dest: 'robots.txt' },
+          { src: 'style.css', dest: 'style.css' }
+        ]
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-s3');
 
   grunt.registerTask('default', ['watch']);
 };
