cbe3840128ff588120e8d71955d0f61600e36bf6
Zoltan Olah <zol@percolatestudio.com>
2014-08-29 17:01:48 -0700

    Adds our own social jquery plugin for sharing

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
 $(function() {
   FastClick.attach(document.body);
+
+  var url = 'http://meetsous.com';
+
+  $('[data-facebook=button]').social({
+    network: 'facebook',
+    counter: '[data-facebook=counter]',
+    url: url
+  });
+  
+  $('[data-twitter=button]').social({
+    network: 'twitter',
+    counter: '[data-twitter=counter]',
+    url: url
+  });
+  
+  $().social({
+    network: 'github',
+    counter: '[data-github=counter]',
+    prefix: 'meteor', //XXX: changeme, github user
+    url: 'meteor' //XXX: changeme, github repo
+  });
+
 });
